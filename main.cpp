9dd20df8c511452b06c41ae169c9f3a608d2d387
Elie Michel <elie.michel@exppad.com>
2024-01-31 08:58:46 +0100

    Add missing releases

--- main.cpp
+++ main.cpp
@@ -298,7 +298,9 @@ fn fs_main() -> @location(0) vec4f {
 		CommandBufferDescriptor cmdBufferDescriptor;
 		cmdBufferDescriptor.label = "Command buffer";
 		CommandBuffer command = encoder.finish(cmdBufferDescriptor);
+		encoder.release();
 		queue.submit(command);
+		command.release();
 
 		swapChain.present();
 
@@ -308,10 +310,13 @@ fn fs_main() -> @location(0) vec4f {
 #endif
 	}
 
+	pipeline.release();
+	shaderModule.release();
 	swapChain.release();
 	device.release();
 	adapter.release();
 	instance.release();
+	surface.release();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
